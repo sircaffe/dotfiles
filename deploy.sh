1316ceae2340db3b96d0bf60e19a1b6057e47e45
janregx <kinanprincipes@gmail.com>
2024-10-02 14:44:50 -0400

    Refactored [backup_dotfiles.sh] and [common.sh]

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,31 @@ source common.sh
 
 allowall=0
 
+list=(
+    .emacs.local
+    .emacs
+    .emacs.custom.el
+)
+
+# Logging Functions
+log() {
+    echo -e "[\e[32mLOG\e[0m]: $1"
+}
+err() {
+    echo -e "[\e[31mERROR\e[0m]: $1"
+}
+warn() {
+    echo -e "[\e[33mWARNING\e[0m]: $1"
+}
+
+
 make_syml() { ln -sf $(pwd)/"$i" $HOME/"$i"; }
 
 for i in "${list[@]}"; do
     if [ -L "$HOME/$i" ]; then
         if [ $allowall -eq 0 ]; then
-            warn "\"\e[1m$i\e[0m\" already exists."
-            read -p "Overwrite? (y/n/a):" -n 1 -r
+            warn "\e[1m$i\e[0m already exists."
+            read -p "Overwrite? y/n/a: " -n 1 -r
             echo
         fi
 
