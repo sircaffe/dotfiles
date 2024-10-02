10956c57ce22ac032c338a85c3b335afd034ed85
janregx <kinanprincipes@gmail.com>
2024-10-02 14:34:47 -0400

    Configs are now symlinks to dotfiles instead of copies

--- deploy.sh
+++ deploy.sh
@@ -4,27 +4,31 @@ source common.sh
 
 allowall=0
 
+make_syml() { ln -sf $(pwd)/"$i" $HOME/"$i"; }
+
 for i in "${list[@]}"; do
-    if [ -f "$HOME/$i" ] || [ -d "$HOME/$i" ]; then
+    if [ -L "$HOME/$i" ]; then
         if [ $allowall -eq 0 ]; then
             warn "\"\e[1m$i\e[0m\" already exists."
-            read -p "Skip? (y/n/a):" -n 1 -r
+            read -p "Overwrite? (y/n/a):" -n 1 -r
             echo
         fi
 
         if [[ $REPLY = "y" ]]; then
-            warn "Overwriting $i"
-            cp ./"$i" $HOME/"$i"
+            log "Deleting $i and creating corresponding symlink."
+            make_syml
         elif [[ $REPLY = "n" ]]; then
             warn "Skipping $i..."
             continue
         elif [[ $REPLY = "a" ]]; then
             allowall=1
-            warn "Overwriting $i"
-            cp ./"$i" $HOME/"$i"
+            log "Deleting $i and creating corresponding symlink."
+            make_syml
         else
             echo Abort.
             break
         fi
+    else
+	make_syml	
     fi
 done
